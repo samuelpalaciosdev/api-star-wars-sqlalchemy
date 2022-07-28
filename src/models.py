0fb8aa139adfda57ce141ba87a7d0915c3b5d19b
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-28 15:37:12 -0400

    Add character model

--- src/models.py
+++ src/models.py
@@ -13,5 +13,33 @@ class User(db.Model):
         return{
             'id': self.id,
             'username': self.username,
+            'is_active': self.is_active
             # do not serialize password for security purposes
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    image = db.Column(db.String(250), default="https://www.prensalibre.com/wp-content/uploads/2019/11/luke-skywalker-644x362.jpg?quality=52")
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    birth_year = db.Column(db.Integer)
+    gender = db.Column(db.String(50))
+    # homeworld = db.Column(db.Integer, ForeignKey('planets.id'), default='Not known')
+
+    
