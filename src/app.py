30d3ae6e9d4eae6ebf42ca6196006952664a0eed
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-30 16:06:35 -0400

    Add planets, vehicles and users GET functionalities

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from models import db, User, Character, Planet, Vehicle, favorite_characters, favorite_planets, favorite_vehicles
@@ -11,11 +12,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
 db.init_app(app)
 Migrate(app, db) # db init, db migrate, db upgrade
 
-#### code inside this lines ####
+#### Main Route ####
 
-@app.route('/') # Main Page
+@app.route('/') 
 def main():
     return jsonify({ "msg": "Welcome to Star Wars API!!!" }), 200
+
+#### Characters Route ####
 
 # Returns list of all the characters in the Database
 @app.route('/api/characters', methods=['GET'])
@@ -24,14 +27,51 @@ def get_characters():
     characters = list(map(lambda character: character.serialize(), characters))
     return jsonify(characters), 200
 
-
-# Returns info about a character based on its id
-
+# Returns info about a character by id
 @app.route('/api/characters/<int:character_id>', methods=['GET'])
 def get_character_by_id(character_id):
-    characters = Character.query.get(character_id)
-    return jsonify(characters.serialize()), 200
+    character = Character.query.get(character_id)
+    return jsonify(character.serialize()), 200
 
+
+#### Planets Route ####
+
+# Returns list of all the planets in the Database
+@app.route('/api/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(planets), 200
+
+# Returns info about a planet by id
+@app.route('/api/planets/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    planet = Planet.query.get(planet_id)
+    return jsonify(planet.serialize()), 200
+
+#### Vehicles Route ####
+
+# Returns list of all the vehicles in the Database
+@app.route('/api/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicle.query.all()
+    vehicles = list(map(lambda vehicle: vehicle.serialize(), vehicles))
+    return jsonify(vehicles), 200
+
+# Returns info about a vehicle by id
+@app.route('/api/vehicles/<int:vehicle_id>', methods=['GET'])
+def get_vehicle_by_id(vehicle_id):
+    vehicle = Vehicle.query.get(vehicle_id)
+    return jsonify(vehicle.serialize()), 200
+
+#### Users Route ####
+
+# Returns list of all the users in the Database
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
 
 
 
